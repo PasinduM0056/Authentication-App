a12e762686c953b78845f3394c6d71e047b5ea30
PasinduM0056 <malindafiv56@gmail.com>
2024-09-23 11:06:40 +0530

    sign in controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,5 +37,38 @@ exports.signup = async (req, res, next) => {
 
 //LOGIN USER
 exports.login = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
 
+        const user = await User.findOne({email});
+
+        if(!user) return next(new createError("User not found!", 404));
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return next(new createError('Invalid email or password', 401));
+        }
+
+         //Assign JWT (json web token) to user
+       const token = jwt.sign({ _id: user._id}, 'secretkey123', {
+        expiresIn: '90d',
+       });
+
+       res.status(200).json({
+        status: 'success',
+        token,
+        message: 'Logged in successfully',
+        user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        }
+       })
+
+
+    } catch (error) {
+        next(error)
+    }
 }
